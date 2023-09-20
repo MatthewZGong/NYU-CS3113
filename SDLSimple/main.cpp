5e2913912692160e12c416881558863eea28037f
MatthewZGong <matthewzgong@gmail.com>
2023-09-20 16:53:43 -0400

    Initial Commit

---
+++ SDLSimple/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDLSimple
+//
+//  Created by Matthew Gong on 9/20/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
